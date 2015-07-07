41e09b97188fbe3e6ffd83f1d9d798fee97212cd
Jesse Zelaya <jessezelaya1@gmail.com>
2015-07-07 15:44:19 -0700

    built animate and sound functions

--- script/app.js
+++ script/app.js
@@ -1,45 +1,52 @@
-$(function(){
+
 
 var computerSequence = [];
 var playerSequence = [];
 var round = 0;
-
-//ADD EVENT LISTENER TO COLORED SQUARES
-$('.square').click(function(){
-  console.log(this);
-});
-
-//ADD EVENT LISTENER TO RESET
-$('#reset').click(function () {
-  console.log(this);
-});
-
-//ADD EVENT LISTENER TO CHALLENGE MODE
-$('#challenge_mode').click(function () {
-  console.log(this);
-});
+var simon;
 
 
-//1. WHEN START BUTTON IS CLICKED START THE GAME
-$('#start').click(generateNum);
+// WHEN START BUTTON IS CLICKED START THE GAME
+$('#start').click(randomColor);
 
-  //1a. GAME WILL GENERATE RANDOM NUMBER
-  function generateNum() {
+
+// GAME WILL GENERATE RANDOM Color
+  function randomColor() {
     var randomNumber = Math.floor(Math.random() * 9);
-      //a.IF NUMBER <= 2 IT WILL REFER TO RED SQUARE
+      //IF NUMBER <= 2 IT WILL REFER TO RED SQUARE
       if (randomNumber <= 2) {
-        computerSequence.push('RED');
-      //b. IF NUMBER <= 4 IT WILL REFER TO GREEN SQUARE
+        computerSequence.push('red');
+      // IF NUMBER <= 4 IT WILL REFER TO GREEN SQUARE
       }else if (randomNumber <= 4) {
-        computerSequence.push('GREEN');
-      //c. IF NUMBER <= 6 IT WILL REFER TO BLUE SQUARE
+        computerSequence.push('green');
+      // IF NUMBER <= 6 IT WILL REFER TO BLUE SQUARE
       }else if (randomNumber <= 6) {
-          computerSequence.push('BLUE');
-      //d. IF NUMBER <= 8 IT WILL REFER TO YELLOW SQUARE
+          computerSequence.push('blue');
+      // IF NUMBER <= 8 IT WILL REFER TO YELLOW SQUARE
       }else {
-          computerSequence.push('YELLOW');
+          computerSequence.push('yellow');
       }
       console.log(computerSequence);
   }
 
-});
+
+  $('.square').click(playerClick);
+
+  //PUSH PLAYERS SEQUENCE INTO AN ARRAY
+  function playerClick() {
+    playerSequence.push(this.id);
+    console.log(playerSequence);
+  }
+
+  //ANIMATE COLORED SQUARE
+  function animate() {
+    var square = $(this);
+    square.animate({opacity: '0.2' }, "fast");
+    square.animate({opacity: '1' }, 'fast');
+    // width: '200px', height: '200px'
+  }
+
+  //PLAY SOUND WHEN SQUARE IS CLICKED
+  function playSound() {
+    $('audio',this).trigger('play');
+  }
