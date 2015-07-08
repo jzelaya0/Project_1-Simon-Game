4c0050169b087784feceac96a6ff174fc874c70f
Jesse Zelaya <jessezelaya1@gmail.com>
2015-07-07 19:02:04 -0700

    animate function called in player and random function

--- script/app.js
+++ script/app.js
@@ -16,49 +16,52 @@ $('#start').click(randomColor);
       //IF NUMBER <= 2 IT WILL REFER TO RED SQUARE
       if (randomNumber <= 2) {
         computerSequence.push('red');
+        animate($('#red'));
       // IF NUMBER <= 4 IT WILL REFER TO GREEN SQUARE
       }else if (randomNumber <= 4) {
         computerSequence.push('green');
+        animate($('#green'));
       // IF NUMBER <= 6 IT WILL REFER TO BLUE SQUARE
       }else if (randomNumber <= 6) {
           computerSequence.push('blue');
+          animate($('#blue'));
       // IF NUMBER <= 8 IT WILL REFER TO YELLOW SQUARE
       }else {
           computerSequence.push('yellow');
+          animate($('#yellow'));
       }
       console.log(computerSequence);
   }
 
 
-  $('.square').click(animate);
+  $('.square').click(playerClick);
 
   //PUSH PLAYERS SEQUENCE INTO AN ARRAY
   function playerClick() {
-    var name = this;
     playerSequence.push(this.id);
     console.log(playerSequence);
-
+    animate(this);
   }
 
-  function animate() {
-    $(this).animate({opacity: '0.2' }, 'fast');
-    $(this).animate({opacity: '1' }, 'fast');
-    $('audio',this).trigger('play');
-    console.log(name);
-  }
-
-
-  function newRound() {
-    if (simon === true) {
-      //CONTINUE TO PLAY GAME
-    }
+  function animate(arg) {
+    $(arg).animate({opacity: '0.2' }, 'fast');
+    $(arg).animate({opacity: '1' }, 'fast');
+    $('audio',arg).trigger('play');
   }
 
 
-  function checkMatch() {
-    if (computerSequence == playerSequence) {
-      //START A NEW ROUND
-    }else {
-      //RESTART THE GAME
-    }
-  }
+
+  // function newRound() {
+  //   if (simon === true) {
+  //     //CONTINUE TO PLAY GAME
+  //   }
+  // }
+  //
+  //
+  // function checkMatch() {
+  //   if (computerSequence == playerSequence) {
+  //     //START A NEW ROUND
+  //   }else {
+  //     //RESTART THE GAME
+  //   }
+  // }
