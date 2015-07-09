d7bf67b88342443e019c86fc01b2596ee945eb9c
Jesse Zelaya <jessezelaya1@gmail.com>
2015-07-09 14:35:51 -0700

    Simon game is functional

--- script/app.js
+++ script/app.js
@@ -2,6 +2,8 @@
 
 var computerSequence = [];
 var playerSequence = [];
+var resetPlayerSequence;
+var currentClickCount = 0;
 var round = 0;
 var simon = true;
 
@@ -11,16 +13,16 @@ $('#start').click(startGame);
 
 function startGame() {
   //START THE FIRST ROUND
-  // console.log("Game will Begin");
+  console.log("Game will Begin");
   randomColor();
+  playPattern();
 }
 
 
 
 function newRound() {
-
-  //Animate color patterh for computer sequence
-
+  //Animate color pattern for computer sequence
+  playPattern();
 }
 
 
@@ -29,61 +31,73 @@ function checkForMatch() {
   //Will check if computer and player pattern matches
   //If there is a match... move to a new round.
   //If not then the game will end.
-  if (computerSequence.join() == playerSequence.join()) {
-      alert('Good Move');
-  }else {
-      alert('Bad Move');
+    var clickCountCheck = computerSequence.length;
+    console.log(clickCountCheck + '=' + currentClickCount);
+    if(clickCountCheck == currentClickCount){
+      if (computerSequence.join() == playerSequence.join()){
+        newRound();
+        randomColor();
+        playerSequence = [];
+        currentClickCount = 0;
+        console.log('Computer: ' + computerSequence);
+         alert("Good!");
+      }else {
+        currentClickCount = 0;
+        alert('You Lose!');
+      }
   }
-
 }
 
+
+
 //COMPUTER GENERATES PATTERN
-function playPattern(computer) {
+function playPattern() {
   var i = 0;
   var square = this;
   var interval = setInterval(function(){
-    square.animate($('#' + computer[i]));
+    square.animate($(computerSequence[i]));
 
     i++;
-    if (i >= computer.length) {
+    if (i >= computerSequence.length) {
       clearInterval(interval);
     }
   },700);
 }
 
 
-
-// GAME WILL GENERATE RANDOM Color
+// GAME WILL GENERATE RANDOM COLOR
   function randomColor() {
     var randomNumber = Math.floor(Math.random() * 9);
       //IF NUMBER <= 2 IT WILL REFER TO RED SQUARE
       if (randomNumber <= 2) {
-        computerSequence.push('red');
-        animate($('#red'));
+        computerSequence.push('#red');
+        // animate($('#red'));
       // IF NUMBER <= 4 IT WILL REFER TO GREEN SQUARE
       }else if (randomNumber <= 4) {
-        computerSequence.push('green');
-        animate($('#green'));
+        computerSequence.push('#green');
+        // animate($('#green'));
       // IF NUMBER <= 6 IT WILL REFER TO BLUE SQUARE
       }else if (randomNumber <= 6) {
-          computerSequence.push('blue');
-          animate($('#blue'));
+          computerSequence.push('#blue');
+          // animate($('#blue'));
       // IF NUMBER <= 8 IT WILL REFER TO YELLOW SQUARE
       }else {
-          computerSequence.push('yellow');
-          animate($('#yellow'));
+          computerSequence.push('#yellow');
+          // animate($('#yellow'));
       }
-      console.log(computerSequence);
   }
 
 
   $('.square').click(playerClick);
   //PUSH PLAYERS SEQUENCE INTO AN ARRAY
   function playerClick() {
-    playerSequence.push(this.id);
+    playerSequence.push("#" +this.id);
+    console.log("Player: " + playerSequence);
     animate(this);
-    console.log(playerSequence);
+    currentClickCount+=1;
+    checkForMatch();
   }
+
   //ANIMATES BOTH COMPUTER AND PLAYER MOVES
   function animate(arg) {
     $(arg).animate({opacity: '0.2' }, 100);
