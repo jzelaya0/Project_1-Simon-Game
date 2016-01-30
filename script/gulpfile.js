053cc650daa2ba9cdcb7c65a2ed31a1c1cc5c046
Jesse Zelaya <jessezelaya1@gmail.com>
2016-01-30 15:24:11 -0800

    SIMON-Build-Tasks - Created default task to log gulp is running

---
+++ script/gulpfile.js
@@ -0,0 +1,11 @@
+// REQUIRE DEPENDENCIES
+// =========================
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+
+// DEFAULT TASKS
+// =========================
+gulp.task("default", function(){
+  return gutil.log("GULP IS RUNNING...");
+});
