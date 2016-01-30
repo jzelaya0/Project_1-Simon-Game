6f80838aa102762f85cc05f9b44c1eccc4dc7882
Jesse Zelaya <jessezelaya1@gmail.com>
2016-01-30 15:56:31 -0800

    SIMON-Build-Tasks - Set up jshint

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,23 @@
 // =========================
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 
 
-// DEFAULT TASKS
+// DEFAULT TASK
 // =========================
-gulp.task("default", function(){
-  return gutil.log("GULP IS RUNNING...");
+gulp.task("default", ['watch']);
+
+// TASKS
+// =========================
+
+//Jshint task config
+gulp.task('jshint', function(){
+  return gulp.src("source/javascript/*.js")
+    .pipe(jshint())
+    .pipe(jshint.reporter("jshint-stylish"));
 });
+
+gulp.task('watch', function(){
+  gulp.watch("source/javascript/*.js", ["jshint"]);
+})
