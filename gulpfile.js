3d7bae54ae7b8ea2bb71e2de64429750c78e8f51
Jesse Zelaya <jessezelaya1@gmail.com>
2016-01-30 16:49:58 -0800

    SIMON-Build-Tasks - Set up gulp to concat and minify JS files

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var gutil      = require('gulp-util');
 var jshint     = require('gulp-jshint');
 var stylus     = require('gulp-stylus');
 var sourcemaps = require('gulp-sourcemaps');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var gulpIf     = require('gulp-if');
 
 
 // DEFAULT TASK
@@ -31,9 +34,20 @@ gulp.task('build-css', function(){
     .pipe(gulp.dest("public/assets/css"));
 });
 
+//Javascript task config
+gulp.task('build-js',function(){
+  return gulp.src("source/javascript/*js")
+    .pipe(sourcemaps.init())//process original sources
+    .pipe(concat("bundle.js"))
+    .pipe(gulpIf('*.js', uglify()))//minifies only if its a JS file
+    .pipe(sourcemaps.write())//add map to modified source
+    .pipe(gulp.dest("public/assets/scripts"));
+});
+
 // WATCH FOR CHANGES
 // ==================================================
 gulp.task('watch', function(){
   gulp.watch("source/javascript/*.js", ["jshint"]);
+  gulp.watch("source/javascript/*.js", ["build-js"]);
   gulp.watch("source/stylus/*.styl", ["build-css"]);
 });
