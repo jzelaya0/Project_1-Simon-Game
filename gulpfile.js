4a51d511c57da34122087b994178569c2f093b2a
Jesse Zelaya <jessezelaya1@gmail.com>
2016-02-03 10:51:18 -0800

    SIMON-Media-Queries Set task to rename file to minified file

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var sourcemaps      = require('gulp-sourcemaps');
 var concat          = require('gulp-concat');
 var uglify          = require('gulp-uglify');
 var gulpIf          = require('gulp-if');
+var rename          = require('gulp-rename');
 var browserSync     = require('browser-sync').create();
 
 
@@ -46,6 +47,7 @@ gulp.task('build-css', function(){
     .pipe(sourcemaps.init())//process original sources
     .pipe(stylus({compress: true}))
     .pipe(sourcemaps.write())//add map to modified source
+    .pipe(rename('main.min.css'))
     .pipe(gulp.dest(output.css)).on("error", gutil.log)
     .pipe(browserSync.reload({
       stream: true
