6269971f0769373a458a0edaaffcfbf1cd1dce56
Jesse Zelaya <jessezelaya1@gmail.com>
2016-02-03 17:48:48 -0800

    SIMON-Modals Created html conent in jquery objects

--- source/javascript/modal.js
+++ source/javascript/modal.js
@@ -1,1 +1,15 @@
-console.log("Hello from modal.js");
+// ##############################
+// TO DO
+// ##############################
+// 1. Create variables with html content to append to the body
+// 2. Create a function to open the modals
+  // 2a. Must be reusable for Game Over alert and Challenge Mode alert
+// 3. Create a function to close the modal on click
+// 4. Append messages to the modal content
+
+// MODAL ELEMENTS
+// ==================================================
+var $overlay = $('<div id="overlay"></div>');
+var $modal = $('<div id="modal"></div>');
+var $modalContent =  $('<div id="modal-content"></div>');
+var $modalClose = $('<div id="modal-close"></div>');
